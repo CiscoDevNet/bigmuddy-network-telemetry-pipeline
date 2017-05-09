18e56bdf10994386c817e302a2923e2dd0f836b0
nleiva <nicolasleiva@gmail.com>
2017-05-09 14:54:34 -0400

    Add some proto files for Cisco-IOS-XR-plat-chas-invmgr-oper

---
+++ vendor/github.com/cisco/bigmuddy-network-telemetry-proto/proto_go/cisco_ios_xr_plat_chas_invmgr_oper/platform_inventory/racks/rack/slots/slot/cards/card/attributes/basic_info/a.go
@@ -0,0 +1,6 @@
+
+//go:generate protoc --go_out=plugins=grpc:. inv_xml_entity_basic_info.proto
+        
+// Cisco-IOS-XR-plat-chas-invmgr-oper:platform-inventory/racks/rack/slots/slot/cards/card/attributes/basic-info
+package cisco_ios_xr_plat_chas_invmgr_oper_platform_inventory_racks_rack_slots_slot_cards_card_attributes_basic_info
+            
---
+++ vendor/github.com/cisco/bigmuddy-network-telemetry-proto/proto_go/cisco_ios_xr_plat_chas_invmgr_oper/platform_inventory/racks/rack/slots/slot/cards/card/port_slots/port_slot/portses/ports/attributes/basic_info/a.go
@@ -0,0 +1,6 @@
+
+//go:generate protoc --go_out=plugins=grpc:. inv_xml_entity_basic_info.proto
+        
+
+package cisco_ios_xr_plat_chas_invmgr_oper_platform_inventory_racks_rack_slots_slot_cards_card_portses_ports_attributes_basic_info
+            
