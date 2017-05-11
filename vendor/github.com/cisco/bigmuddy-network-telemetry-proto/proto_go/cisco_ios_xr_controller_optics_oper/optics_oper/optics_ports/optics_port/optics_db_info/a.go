4cb91e010a359aa60febb84a17114b6bf61da658
nleiva <nicolasleiva@gmail.com>
2017-05-11 19:10:38 -0400

    Add proto for optics-db-info and optics-lane from Cisco-IOS-XR-controller-optics-oper

---
+++ vendor/github.com/cisco/bigmuddy-network-telemetry-proto/proto_go/cisco_ios_xr_controller_optics_oper/optics_oper/optics_ports/optics_port/optics_db_info/a.go
@@ -0,0 +1,6 @@
+
+//go:generate protoc --go_out=plugins=grpc:. optics_db_info.proto
+        
+// Cisco-IOS-XR-controller-optics-oper:optics-oper/optics-ports/optics-port/optics-db-info
+package cisco_ios_xr_controller_optics_oper_optics_oper_optics_ports_optics_port_optics_db_info
+            
