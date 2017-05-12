0ccb49f8e8d4ad5e604d86787e4521f99bb95e85
nleiva <nicolasleiva@gmail.com>
2017-05-12 13:23:22 -0400

    Add proto file for Cisco-IOS-XR-fretta-bcm-dpa; npu-stats-oper and hw-resources-oper

---
+++ vendor/github.com/cisco/bigmuddy-network-telemetry-proto/proto_go/cisco_ios_xr_fretta_bcm_dpa_hw_resources_oper/dpa/stats/nodes/node/hw_resources_datas/hw_resources_data/a.go
@@ -0,0 +1,6 @@
+
+//go:generate protoc --go_out=plugins=grpc:. dpa_sysdb_hw_resource.proto
+        
+// Cisco-IOS-XR-fretta-bcm-dpa-hw-resources-oper:dpa/stats/nodes/node/hw-resources-datas/hw-resources-data
+package cisco_ios_xr_fretta_bcm_dpa_hw_resources_oper_dpa_stats_nodes_node_hw_resources_datas_hw_resources_data
+            
