3e1611e29dd12bd485830546bd6cd7645c2e3e96
nleiva <nicolasleiva@gmail.com>
2017-05-12 20:43:28 -0400

    Add proto files for Cisco-IOS-XR-alarmgr-server-oper

---
+++ vendor/github.com/cisco/bigmuddy-network-telemetry-proto/proto_go/cisco_ios_xr_alarmgr_server_oper/alarms/detail/detail_system/active/a.go
@@ -0,0 +1,6 @@
+
+//go:generate protoc --go_out=plugins=grpc:. alarm_mgr_show_alarm_info_cli.proto
+        
+// Cisco-IOS-XR-alarmgr-server-oper:alarms/detail/detail-system/active
+package cisco_ios_xr_alarmgr_server_oper_alarms_detail_detail_system_active
+            
