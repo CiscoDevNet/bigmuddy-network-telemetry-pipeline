f7801ee26925e39ff4e7ca9c462f68870b0cb557
nleiva <nicolasleiva@gmail.com>
2017-05-13 00:17:55 -0400

    Add fib-summary proto file for Cisco-IOS-XR-fib-common-oper

---
+++ vendor/github.com/cisco/bigmuddy-network-telemetry-proto/proto_go/cisco_ios_xr_fib_common_oper/fib/nodes/node/protocols/protocol/fib_summaries/fib_summary/a.go
@@ -0,0 +1,6 @@
+
+//go:generate protoc --go_out=plugins=grpc:. fib_sh_sum.proto
+        
+// Cisco-IOS-XR-fib-common-oper:fib/nodes/node/protocols/protocol/fib-summaries/fib-summary
+package cisco_ios_xr_fib_common_oper_fib_nodes_node_protocols_protocol_fib_summaries_fib_summary
+            
